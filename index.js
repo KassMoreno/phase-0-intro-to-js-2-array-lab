911cdb2933e5805377fb360bab9ad40f0a87576c
Kassandra <K1995Moreno@gmail.com>
2022-11-01 13:21:51 -0400

    commit tree 183b71091e7c4fc321967490419d69d3dfa7e8d9

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Write your solution here!
+//Create List
+const cats = ["Milo", "Otis", "Garfield"]
+
+//Destructively Append
+function destructivelyAppendCat(addEnd){
+    return cats.push(addEnd)
+}
+
+//Destructively Prepend
+function destructivelyPrependCat(addFront){
+    return cats.unshift(addFront)
+}
+
+//Destructively Remove Last
+function destructivelyRemoveLastCat(){
+    return cats.pop([-1])
+}
+
+//Destructively Remove First
+function destructivelyRemoveFirstCat(){
+    return cats.shift([0])}
+
+//Nondestructive Append
+function appendCat(newAdd){
+    let newCatsAdd=[...cats,newAdd]
+    return newCatsAdd
+}
+
+//Nondestructive Prepend
+function prependCat(addNew){
+    let newAddCats=[addNew,...cats]
+    return newAddCats
+}
+    
+//Nondestructively Remove Last
+function removeLastCat(){
+    let newCatsDel= cats.slice(0,cats.length -1)
+    return newCatsDel
+}
+
+function removeFirstCat(){
+    let newDelCats= cats.slice(1, cats.length)
+    return newDelCats
+}
